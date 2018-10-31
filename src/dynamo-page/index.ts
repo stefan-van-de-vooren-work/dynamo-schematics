eb58e148dfc8f3ce30c9bac72882e69397e9ccf8
Jorgen Leijenaar <jorgen.leijenaar@rabobank.com>
2018-10-31 15:56:19 +0100

    dynamo-page schematic: refactorings

--- src/dynamo-page/index.ts
+++ src/dynamo-page/index.ts
@@ -1,6 +1,6 @@
 import * as ts from 'typescript';
 
-import { Path, join, normalize, strings } from '@angular-devkit/core';
+import {Path, join, normalize, strings} from '@angular-devkit/core';
 
 import {
     DirEntry,
@@ -18,47 +18,27 @@ import {
     url,
 } from '@angular-devkit/schematics';
 
-import { findNodes } from '@schematics/angular/utility/ast-utils';
-import { Change, InsertChange } from '@schematics/angular/utility/change';
-import { getWorkspace } from '@schematics/angular/utility/config';
-import { ModuleOptions, buildRelativePath } from '@schematics/angular/utility/find-module';
-import { parseName } from '@schematics/angular/utility/parse-name';
-import { validateHtmlSelector, validateName } from '@schematics/angular/utility/validation';
+import {findNodes} from '@schematics/angular/utility/ast-utils';
+import {Change, InsertChange} from '@schematics/angular/utility/change';
+import {getWorkspace} from '@schematics/angular/utility/config';
+import {buildRelativePath} from '@schematics/angular/utility/find-module';
+import {parseName} from '@schematics/angular/utility/parse-name';
+import {validateHtmlSelector, validateName} from '@schematics/angular/utility/validation';
 
-import { Schema as PageOptions } from './schema';
+import {Schema as PageOptions} from './schema';
 
 const pagesRoot = './libs/pages/';
 
-function findRoutingModuleFromOptions(host: Tree, project: any, options: ModuleOptions): Path | undefined {
-    if (options.hasOwnProperty('skipImport') && options.skipImport) {
+function findRoutingModuleFromOptions(host: Tree, project: any, options: PageOptions): Path | undefined {
+    if (options.hasOwnProperty('addRoute') && !options.addRoute) {
         return undefined;
     }
-
-    if (!options.module) {
-
-        const pathToCheck = `/${project.root}/src/app`
-            + (options.flat ? '' : '/' + strings.dasherize(options.name));
-        return normalize(findRoutingModule(host, pathToCheck));
-    } else {
-        const modulePath = normalize(
-            '/' + (options.path) + '/' + options.module);
-        const moduleBaseName = normalize(modulePath).split('/').pop();
-
-        if (host.exists(modulePath)) {
-            return normalize(modulePath);
-        } else if (host.exists(modulePath + '.ts')) {
-            return normalize(modulePath + '.ts');
-        } else if (host.exists(modulePath + '.module.ts')) {
-            return normalize(modulePath + '.module.ts');
-        } else if (host.exists(modulePath + '/' + moduleBaseName + '.module.ts')) {
-            return normalize(modulePath + '/' + moduleBaseName + '.module.ts');
-        } else {
-            throw new Error('Specified module does not exist');
-        }
-    }
+    const pathToCheck = `/${project.root}/src/app`
+        + (options.flat ? '' : '/' + strings.dasherize(options.name));
+    return normalize(findRoutingModule(host, pathToCheck, options.name));
 }
 
-function findRoutingModule(host: Tree, generateDir: string): Path {
+function findRoutingModule(host: Tree, generateDir: string, projectName: string): Path {
     let dir: DirEntry | null = host.getDir('/' + generateDir);
 
     const routingModuleRe = /-routing\.module\.ts/;
@@ -69,33 +49,32 @@ function findRoutingModule(host: Tree, generateDir: string): Path {
         if (matches.length === 1) {
             return join(dir.path, matches[0]);
         } else if (matches.length > 1) {
-            throw new Error('More than one module matches. Use skip-import option to skip importing '
-                + 'the component into the closest module.');
+            throw new Error('More than one app-routing.module matches. Use --addRoute=false option to skip adding '
+                + 'the route to the closest app-routing.module.');
         }
 
         dir = dir.parent;
     }
 
-    throw new Error('Could not find an NgModule. Use the skip-import '
-        + 'option to skip importing in NgModule.');
+    throw new Error(`Project ${projectName} does not contain a app-routing.module.ts`);
 }
 
 function addRouteToNgModule(options: PageOptions): Rule {
-    const { module } = options;
+    const {routingModule} = options;
 
-    if (!module) {
-        throw new SchematicsException('module option is required.');
+    if (!routingModule) {
+        return host => { return host};
     }
 
     return host => {
-        const text = host.read(module);
+        const text = host.read(routingModule);
 
         if (!text) {
-            throw new SchematicsException(`File ${module} does not exist.`);
+            return host;
         }
 
         const sourceText = text.toString('utf8');
-        const source = ts.createSourceFile(module, sourceText, ts.ScriptTarget.Latest, true);
+        const source = ts.createSourceFile(routingModule, sourceText, ts.ScriptTarget.Latest, true);
 
         const pagePath = (
             `/${options.path}/` +
@@ -103,12 +82,12 @@ function addRouteToNgModule(options: PageOptions): Rule {
             `${strings.dasherize(options.name)}.module`
         );
 
-        const relativePath = buildRelativePath(module, pagePath);
+        const relativePath = buildRelativePath(routingModule, pagePath);
 
         const routePath = options.routePath ? options.routePath : options.name;
         const routeLoadChildren = `${relativePath}#${strings.classify(options.name)}PageModule`;
-        const changes = addRouteToRoutesArray(source, module, routePath, routeLoadChildren);
-        const recorder = host.beginUpdate(module);
+        const changes = addRouteToRoutesArray(source, routingModule, routePath, routeLoadChildren);
+        const recorder = host.beginUpdate(routingModule);
 
         for (const change of changes) {
             if (change instanceof InsertChange) {
@@ -128,10 +107,10 @@ function addRouteToRoutesArray(source: ts.SourceFile, ngModulePath: string, rout
     for (const keyword of keywords) {
 
         if (ts.isVariableStatement(keyword)) {
-            const [ declaration ] = keyword.declarationList.declarations;
+            const [declaration] = keyword.declarationList.declarations;
 
             if (ts.isVariableDeclaration(declaration) && declaration.initializer && declaration.name.getText() === 'routes') {
-                const  node = declaration.initializer.getChildAt(1);
+                const node = declaration.initializer.getChildAt(1);
                 const lastRouteNode = node.getLastToken();
 
 
@@ -170,7 +149,7 @@ function buildSelector(options: PageOptions, projectPrefix: string) {
     return selector;
 }
 
-export default function(options: PageOptions): Rule {
+export default function (options: PageOptions): Rule {
     return (host, context) => {
 
         options.styleext = "scss";
@@ -182,10 +161,13 @@ export default function(options: PageOptions): Rule {
         const workspace = getWorkspace(host);
         const project = workspace.projects[options.project];
 
+        if(options.addRoute){
+            options.routingModule = findRoutingModuleFromOptions(host, project, options);
+        }
 
-        options.module = findRoutingModuleFromOptions(host, project, options);
+        options.path = pagesRoot + options.project + '/';
 
-        const parsedPath = parseName(pagesRoot, options.name);
+        const parsedPath = parseName(options.path, options.name);
         options.name = parsedPath.name;
         options.path = parsedPath.path;
         options.selector = options.selector ? options.selector : buildSelector(options, project.prefix);
--- src/dynamo-page/schema.ts
+++ src/dynamo-page/schema.ts
@@ -1,12 +1,13 @@
 export interface Schema {
-  path?: string;
-  project: string;
-  name: string;
-  prefix?: string;
-  styleext?: string;
-  spec?: boolean;
-  flat?: boolean;
-  selector?: string;
-  module?: string;
-  routePath?: string;
+    path?: string;
+    project: string;
+    name: string;
+    prefix?: string;
+    styleext?: string;
+    spec?: boolean;
+    flat?: boolean;
+    selector?: string;
+    routingModule?: string;
+    routePath?: string;
+    addRoute?: string;
 }
